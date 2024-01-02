12a1107a175c88e8552c63f3facafbb195def9f9
atiumcache <aattilio@pm.me>
2024-01-02 14:56:40 -0700

    finish pentathlon scoring

--- pentathlon_scoring.py
+++ pentathlon_scoring.py
@@ -1,6 +1,13 @@
 import csv
 from sklearn.linear_model import LinearRegression
 import numpy as np
+import datetime
+
+
+def convert_time_to_seconds(time):
+    split_time = time.strip().split(":")
+    seconds = round((float(split_time[0]) * 60 + float(split_time[1])), 2)
+    return seconds
 
 # Initialize training data 
 # Data gathered from IAAF Scoring Tables of Athletics
@@ -10,27 +17,64 @@ DT_x = np.array([78.39, 67.57, 56.69, 45.76, 34.78]).reshape(-1,1)
 DT_y = np.array([1400, 1200, 1000, 800, 600])
 JT_x = np.array([101.63, 87.53, 73.36, 59.13, 44.82]).reshape(-1,1)
 JT_y = np.array([1400, 1200, 1000, 800, 600])
-run1500_x = np.array([])
+
+    # 1500m times are in seconds, using convert_time function above
+run1500_x = np.array([199.44, 213.2, 228.17, 244.73, 263.52]).reshape(-1,1)
 run1500_y = np.array([1400, 1200, 1000, 800, 600])
 run200_x = np.array([19.8, 20.0, 20.5, 21.0, 22.0, 23.0, 24.0, 25.0]).reshape(-1, 1)
 run200_y = np.array([1252, 1220, 1143, 1068, 925, 793, 671, 560])
 
-# Creating the model
-run200_reg = LinearRegression()
+# Creating the models
+LJ_reg = LinearRegression().fit(LJ_x, LJ_y)
 
-# Fitting the model
-run200_reg.fit(run200_x, run200_y)
+DT_reg = LinearRegression().fit(DT_x, DT_y)
 
-with open('pentathlon_results.csv', 'r+') as csvfile:
+JT_reg = LinearRegression().fit(JT_x, JT_y)
+
+run1500_reg = LinearRegression().fit(run1500_x, run1500_y)
+
+run200_reg = LinearRegression().fit(run200_x, run200_y)
+
+fields = ["Rank","Mark","Competitor","DOB","Nat","Pos","Venue",'Date',"Long Jump","Discus Throw","Javelin Throw","1500m","200m","ResultScore"]
+all_rows = []
+
+# Predict the scores based on the sample data form CSV
+# Then, add each of the 5 scores for a total
+with open('pentathlon_results.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
-    writer = csv.writer(csvfile)
 
     for row in reader:
         try:
-            x_value_200 = np.array([float(row[12])]).reshape(-1, 1)
+            x_value_LJ = np.array([float(row[8])]).reshape(-1, 1)
+            predicted_LJ = LJ_reg.predict(x_value_LJ)
 
-            predicted_200 = run200_reg.predict(x_value)
+            x_value_DT = np.array([float(row[9])]).reshape(-1, 1)
+            predicted_DT = DT_reg.predict(x_value_DT)
+
+            x_value_JT = np.array([float(row[10])]).reshape(-1, 1)
+            predicted_JT = JT_reg.predict(x_value_JT)
+
+            seconds_1500 = convert_time_to_seconds(row[11])
+            x_value_1500 = np.array([seconds_1500]).reshape(-1, 1)
+            predicted_1500 = run1500_reg.predict(x_value_1500)
+
+            x_value_200 = np.array([float(row[12])]).reshape(-1, 1)
+            predicted_200 = run200_reg.predict(x_value_200)
             
+            total_score = predicted_200 + predicted_1500 + predicted_JT + predicted_DT + predicted_LJ
+
+            row[13] = round(total_score[0])
+
+            all_rows.append(row)
+
         except:
             continue
+
+with open('pentathlon_w_scores.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(fields)
+    writer.writerows(all_rows)
     
+    
+
+
--- scratch.py
+++ scratch.py
@@ -1,26 +1,10 @@
+some_times = ["3:19.44", "3:33.20", "3:48.17", "4:04.73", "4:23.52"]
 
+def convert_time_to_seconds(time):
+    split_time = time.strip().split(":")
+    print(split_time)
+    seconds = round((float(split_time[0]) * 60 + float(split_time[1])), 2)
+    return seconds
 
-
-
-
-data1 = "( 10.55/+0.3 - 7.80/+1.2 - 16.00 - 2.05 - 48.42 / 13.75/-1.1 - 50.54 - 5.45 - 71.90 - 4:36.11 )"
-
-ten_records = []
-record = ""
-
-for char in list(data1):
-    
-
-    if char in ["(", "/", ")", " "]: 
-        
-        if record and "+" not in record and "-" not in record:
-            ten_records.append(record)
-            record = ""
-        else:
-            record = ""
-            continue
-        
-    elif char.isnumeric() or char in [".", "+", "-", ":"]:
-        record = "".join([record, char])
-
-print(ten_records)
+for time in some_times:
+    print(convert_time_to_seconds(time))
